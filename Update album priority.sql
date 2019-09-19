6a2b6be6b668cfcd5baba7e80ff15acb767755d7
Pham Hanh <48047723+Joypham@users.noreply.github.com>
2019-09-19 18:47:55 +0700

    Update Update album priority.sql

--- Update album priority.sql
+++ Update album priority.sql
@@ -12,13 +12,13 @@ JOIN (
 	FROM
 		album_track
 	JOIN itunes_album_tracks_release ON album_track.AlbumId = itunes_album_tracks_release.albumuuid
-	AND itunes_album_tracks_release.TrackArtist = itunes_album_tracks_release.Artist
 	AND 
 	(itunes_album_tracks_release.Artist like '%Various Ar%'
 	or 
-	itunes_album_tracks_release.AlbumName like '%NOW That\'s What%')
-where album_track.AlbumPriority <> 2
+	itunes_album_tracks_release.AlbumName like '%NOW That\'s What%'
+	)
+	where album_track.AlbumPriority <> 2
 	GROUP BY
-		itunes_album_tracks_release.AlbumUUID
+itunes_album_tracks_release.AlbumUUID
 ) AS t1 ON t1.albumuuid = album_track.albumid
 SET album_track.AlbumPriority = 2
