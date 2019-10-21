6831bbded19a4d17585b4dee8eda57a71161989d
Pham Hanh <48047723+Joypham@users.noreply.github.com>
2019-10-21 11:16:12 +0700

    Create Checking display status in artist page.sql

---
+++ Checking display status in artist page.sql
@@ -0,0 +1,36 @@
+SELECT * from 
+(
+SELECT 
+t1.*,
+ROW_NUMBER () over (PARTITION BY t1.TrackId ORDER BY FormatID_priority ASC) AS `Rankovertrack`  
+from 
+
+(
+SELECT tracks.Title,tracks.Artist,artists.UUID as artist_UUID,artists.`Name`,artist_track.TrackId,
+datasources.FormatID,
+datasources.Sourceuri,
+if(datasources.FormatID = '74BA994CF2B54C40946EA62C3979DDA3','1','2') as FormatID_priority,
+datasources.DisplayStatus
+from artists
+Join artist_track on artist_track.ArtistId = artists.UUID
+Join tracks on tracks.id = artist_track.TrackId and tracks.Valid > 0
+Join datasources on datasources.TrackId = tracks.Id and datasources.Valid > 0
+and datasources.FormatID in ('74BA994CF2B54C40946EA62C3979DDA3','1A67A5F1E0D84FB9B48234AE65086375')
+
+where 
+artists.name = 'Florida Georgia Line' 
+and 
+artists.Valid > 0
+GROUP BY
+tracks.Id,
+datasources.FormatID,
+datasources.SourceURI
+ORDER BY TrackId
+)
+as t1
+) as t2
+WHERE t2.Rankovertrack = 1
+GROUP BY t2.FormatID,t2.Sourceuri
+ORDER BY 
+t2.TrackID,
+Rankovertrack ASC
