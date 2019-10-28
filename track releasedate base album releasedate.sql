16e0ae3b9b6bb4d99c053b3202e988c072f931e1
Pham Hanh <48047723+Joypham@users.noreply.github.com>
2019-10-28 18:58:45 +0700

    Create track releasedate base album releasedate.sql

---
+++ track releasedate base album releasedate.sql
@@ -0,0 +1,46 @@
+UPDATE tracks
+Join (
+Select * from 
+(SELECT
+	itunes_album_tracks_release.AlbumUUID,
+	itunes_album_tracks_release.AlbumName,
+	itunes_album_tracks_release.Seq,
+	itunes_album_tracks_release.TrackName,
+	itunes_album_tracks_release.TrackArtist,
+	tracks.Id as trackid,
+	albums.Valid,
+	albums.ReleaseDate as album_release_date,
+	tracks.ReleaseDate as tracks_release_date,
+	ROW_NUMBER () over ( PARTITION BY tracks.id ORDER BY tracks.Id, albums.ReleaseDate ASC ) AS `RankOverTrack`
+FROM
+	itunes_album_tracks_release
+JOIN tracks ON tracks.Title = itunes_album_tracks_release.TrackName
+AND tracks.Artist = itunes_album_tracks_release.TrackArtist
+AND tracks.Valid > 0
+and
+tracks.artist = 'ariana grande'
+and
+tracks.id = '7375302F968C4E198B8060301D4B71E8'
+
+AND
+tracks.title not like 'Karaoke Version%'
+and
+tracks.title not like '%Karaoke Version'
+Join albums on albums.uuid =itunes_album_tracks_release.AlbumUUID and albums.valid = 1
+
+and albums.ReleaseDate 
+where 
+itunes_album_tracks_release.Artist <> 'Various Artists'
+and
+itunes_album_tracks_release.AlbumName not like '%NOW That\'s What%' and itunes_album_tracks_release.AlbumName not like '%NOW Party Anthems%'
+) as t1
+where 
+RankOverTrack = 1
+and 
+album_release_date <> tracks_release_date
+order by trackname, trackartist
+) as t3
+On tracks.id = t3.trackid 
+set 
+tracks.ext = JSON_SET(IFNULL(tracks.Ext, JSON_OBJECT()),'$.albumuuid_releasedate',t3.Albumuuid,'$.releasedate',t3.album_release_date)
+
