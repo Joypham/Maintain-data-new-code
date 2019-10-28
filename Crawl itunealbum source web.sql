ffdaae3c374f27bf592f2f6ac5b58a5f71611c64
Pham Hanh <48047723+Joypham@users.noreply.github.com>
2019-10-28 14:08:01 +0700

    Create Crawl itunealbum source web.sql

---
+++ Crawl itunealbum source web.sql
@@ -0,0 +1,4 @@
+* Read me: https://vibbidi.slack.com/archives/GEDMDU5B9/p1572000158008300
+- Lấy albumuuid có trong bảng albums nhưng không có trong bảng itunealbum, mà crawlingtask trả về `look up API hoặc content not found`
+- Lấy albumuuid có trong bảng albums nhưng không có trong bảng itunealbum và ko có dữ liệu trong crawlingtask actionid = 0001892134894FCF83ABCFC1F39E0DE5
+
